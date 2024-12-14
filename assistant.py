7271a6dcbf30dc0bfbadf81a148a9aaa18dd2285
grqphical07 <95062977+grqphical07@users.noreply.github.com>
2024-12-14 12:56:42 -0800

    created basic chat system

--- assistant.py
+++ assistant.py
@@ -2,3 +2,25 @@
 
 A configurable virtual assistant
 """
+
+from groq import Groq
+from dotenv import load_dotenv
+
+MODEL = "llama-3.3-70b-versatile"
+
+load_dotenv()
+
+client = Groq()
+
+prompt = input("What do you want to say?: ")
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": prompt,
+        }
+    ],
+    model=MODEL,
+)
+print(chat_completion.choices[0].message.content)
